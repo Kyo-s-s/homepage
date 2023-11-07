35098081ee1588006828c5c3c2018fb4785791a1
Kyo-s-s <igagrid407@gmail.com>
2023-11-07 20:19:55 +0900

    [Fix] copy button

--- src/app/_components/Pre.tsx
+++ src/app/_components/Pre.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import React, { ReactNode, useState } from "react";
 import { Button } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
 
 interface PreProps {
   children: ReactNode;
@@ -9,14 +9,29 @@ interface PreProps {
 }
 
 const Pre = ({ children, raw, ...props }: PreProps) => {
-  const copyTextToClipboard = () => {
+  const [isCopied, setIsCopied] = useState(false);
+  const copyTextToClipboard = async () => {
     navigator.clipboard.writeText(raw);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
   };
 
   return (
     <>
-      <Button onClick={copyTextToClipboard}>Copy</Button>
-      <pre {...props}>{children}</pre>
+      <pre {...props}>
+        <Button
+          size="sm"
+          top="0.5em"
+          right="0.5em"
+          position="absolute"
+          onClick={copyTextToClipboard}
+        >
+          {isCopied ? "Copied!" : "Copy"}
+        </Button>
+        {children}
+      </pre>
     </>
   );
 };
