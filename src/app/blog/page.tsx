feed704e8fe9c0579f034d14af912543f527a9b3
Kyo-s-s <igagrid407@gmail.com>
2023-11-08 20:59:09 +0900

    [Fix] /blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,29 +1,17 @@
-import Link from "next/link";
 import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts, Post } from "contentlayer/generated";
+import { Box, Container, Heading, Link, LinkBox, Text } from "@chakra-ui/react";
 
-function PostCard(post: Post) {
+const PostCard = (post: Post) => {
   return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-        >
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), "LLLL d, yyyy")}
-      </time>
-      <div
-        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
-        // dangerouslySetInnerHTML={{ __html: post.body.html }}
-        dangerouslySetInnerHTML={{ __html: post.body.code }}
-      />
-    </div>
+    <Box py={2}>
+      <Heading as="h3" size="lg">
+        <Link href={post.url}>{post.title}</Link>
+      </Heading>
+      <Text>{format(parseISO(post.date), "LLLL d, yyyy")}</Text>
+    </Box>
   );
-}
+};
 
 export default function Home() {
   const posts = allPosts.sort((a, b) =>
@@ -31,13 +19,10 @@ export default function Home() {
   );
 
   return (
-    <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">
-        Next.js + Contentlayer Example
-      </h1>
+    <Container maxW="800px">
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
-    </div>
+    </Container>
   );
 }
