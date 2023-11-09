feeea6a90b17cd15eee52e837fc74771b3e88b90
Horike Haruki <igagrid407@gmail.com>
2023-11-09 22:37:34 +0900

    [Add] Affiliation

---
+++ src/app/_components/Affiliation.tsx
@@ -0,0 +1,33 @@
+import { Box, Link, ListItem, UnorderedList } from "@chakra-ui/react";
+import HomeCard from "./HomeCard";
+import { affiliations } from "../_data/affiliations";
+import { ReactNode } from "react";
+
+const Item = ({
+  affiliation,
+}: {
+  affiliation: { title: string; url?: string; details?: ReactNode };
+}) => {
+  const { title, url, details } = affiliation;
+  const linkTitle = url ? <Link href={url}>{title}</Link> : title;
+  return (
+    <>
+      <ListItem>{linkTitle}</ListItem>
+      {details && <Box paddingLeft="0.5em">{details}</Box>}
+    </>
+  );
+};
+
+const Affiliation = () => {
+  return (
+    <HomeCard title="Affiliation">
+      <UnorderedList>
+        {affiliations.map((affiliation, idx) => (
+          <Item affiliation={affiliation} key={idx} />
+        ))}
+      </UnorderedList>
+    </HomeCard>
+  );
+};
+
+export default Affiliation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Profile from "./_components/Profile";
 import Award from "./_components/Award";
+import Affiliation from "./_components/Affiliation";
 
 export default function Home() {
   return (
     <>
       <Profile />
       <Award />
+      <Affiliation />
     </>
   );
 }
