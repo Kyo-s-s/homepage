e90f95b026c2a9ad830436c2b8056851aaf3eb45
Horike Haruki <igagrid407@gmail.com>
2023-11-10 01:32:59 +0900

    [Add] contact

---
+++ src/app/_data/contacts.ts
@@ -0,0 +1,16 @@
+import { FaGithub, FaXTwitter } from "react-icons/fa6";
+
+export const contacts = [
+  {
+    platform: "GitHub",
+    username: "Kyo-s-s",
+    link: "https://github.com/Kyo-s-s",
+    icon: FaGithub,
+  },
+  {
+    platform: "X (Twitter)",
+    username: "@Kyo_s_s",
+    link: "https://twitter.com/Kyo_s_s",
+    icon: FaXTwitter,
+  },
+];
