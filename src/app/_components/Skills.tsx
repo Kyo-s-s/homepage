a007ec745afcf187181b9926bc83530431669753
Kyo-s-s <igagrid407@gmail.com>
2023-11-12 13:17:51 +0900

    [Fix]

--- src/app/_components/Skills.tsx
+++ src/app/_components/Skills.tsx
@@ -1,42 +1,57 @@
 import axios from "axios";
-import { Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import HomeCard from "./HomeCard";
 import { skills } from "../_data/skills";
+import { ReactNode } from "react";
 
 const getAtCoderRate = async (type: "algo" | "heuristic") => {
-  const url = `https://atcoder.jp/users/Kyo_s_s?contestType=${type}`;
+  const url = `https://atcoder.jp/users/Kyo_s_s/history/json?contestType=${type}`;
   const response = await axios.get(url);
-  const html = JSON.stringify(response.data);
-  const match = html.match(/Rating<\/th><td><span class='.+?'>[0-9]+/);
-  if (match !== null) {
-    return Number(match[0].match(/[0-9]+/)![0]);
-  }
-  return null;
+  return (response.data as any[]).map((elem: any) => {
+    return {
+      NewRating: elem["NewRating"] as number,
+    };
+  });
+};
+
+const Skill = ({ title, children }: { title: string; children: ReactNode }) => {
+  return (
+    <Box>
+      <Heading size="md" py={2}>
+        {title}
+      </Heading>
+      <Box pl="1em">{children}</Box>
+    </Box>
+  );
 };
 
 const Skills = async () => {
-  const algoRate = await getAtCoderRate("algo");
-  const heurRate = await getAtCoderRate("heuristic");
+  const algo = await getAtCoderRate("algo");
+  const heur = await getAtCoderRate("heuristic");
+  const algoNowRate = algo[algo.length - 1].NewRating;
+  const heurNowRate = heur[heur.length - 1].NewRating;
+  const algoMaxRate = algo.reduce(
+    (acc, val) => (acc = Math.max(acc, val.NewRating)),
+    0
+  );
+
   return (
     <HomeCard title="Skills">
-      <Heading size="md" pb={2}>
-        AtCoder
-      </Heading>
-      <Text pl="1em">
-        Algorithm: {algoRate} / Heuristic: {heurRate}
-      </Text>
-      <Heading size="md" py={2}>
-        Language
-      </Heading>
-      <Text pl="1em">{skills.language.join(" / ")}</Text>
-      <Heading size="md" py={2}>
-        Framework
-      </Heading>
-      <Text pl="1em">{skills.framework.join(" / ")}</Text>
-      <Heading size="md" py={2}>
-        Tools
-      </Heading>
-      <Text pl="1em">{skills.tools.join(" / ")}</Text>
+      <Skill title="AtCoder">
+        <Text>
+          Algorithm: {algoNowRate} (Max: {algoMaxRate}) / Heuristic:{" "}
+          {heurNowRate}
+        </Text>
+      </Skill>
+      <Skill title="Language">
+        <Text>{skills.language.join(" / ")}</Text>
+      </Skill>
+      <Skill title="Framework">
+        <Text>{skills.framework.join(" / ")}</Text>
+      </Skill>
+      <Skill title="Tools">
+        <Text>{skills.tools.join(" / ")}</Text>
+      </Skill>
     </HomeCard>
   );
 };
