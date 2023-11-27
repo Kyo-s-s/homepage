96b23868f77cf809f7b52a19b302de8996cb4a6e
Kyo-s-s <igagrid407@gmail.com>
2023-11-27 17:47:38 +0900

    [Add] award ICPC 2023

--- src/app/_data/awards.ts
+++ src/app/_data/awards.ts
@@ -2,4 +2,5 @@ export const awards = [
   "ICPC 2022 Asia Yokohama Regional 出場",
   "NASA Space Apps Challenge 2023 横浜大会 最優秀賞",
   "NASA Space Apps Challenge 2023 世界大会 出場",
+  "ICPC 2023 Asia Yokohama Regional 出場",
 ];
