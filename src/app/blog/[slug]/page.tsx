c36d591b1748cd3fb2ddc68bc983b818f2e6a17a
Horike Haruki <igagrid407@gmail.com>
2023-11-27 21:01:32 +0900

    [Add] MdxBody

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,8 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { allPosts } from "contentlayer/generated";
-import type { MDXComponents } from "mdx/types";
-import { useMDXComponent } from "next-contentlayer/hooks";
 import { notFound } from "next/navigation";
-import Pre from "@/app/_components/blog/Pre";
-import LinkHeading from "@/app/_components/blog/LinkHeading";
 import "../../../styles/markdown.css";
+import MdxBody from "@/app/_components/blog/MdxBody";
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -13,28 +10,14 @@ export async function generateStaticParams() {
   }));
 }
 
-const components: MDXComponents = {
-  pre: Pre as any,
-  h1: LinkHeading("h1") as any,
-  h2: LinkHeading("h2") as any,
-  h3: LinkHeading("h3") as any,
-  h4: LinkHeading("h4") as any,
-  h5: LinkHeading("h5") as any,
-  h6: LinkHeading("h6") as any,
-};
-
 export default function Page({ params }: { params: { slug: string } }) {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
 
   if (!post) notFound();
 
-  const MDXContent = useMDXComponent(post.body.code);
-
   return (
     <Box p={4}>
-      <Container maxW="800px">
-        <MDXContent components={components} />
-      </Container>
+      <MdxBody code={post.body.code} />
     </Box>
   );
 }
