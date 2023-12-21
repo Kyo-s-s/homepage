96a8bd3387b4edfcdff6fba890d539461a187a9b
Horike Haruki <igagrid407@gmail.com>
2023-12-22 00:54:07 +0900

    [Fix] move BlogTag

--- src/app/_components/blog/BlogTags.tsx
+++ src/app/_components/blog/BlogTags.tsx
@@ -1,14 +1,5 @@
-import { tagColors } from "@/app/_data/tagColor";
-import { Flex, Link, Tag } from "@chakra-ui/react";
-
-const BlogTag = ({ tag }: { tag: string }) => {
-  return (
-    // TODO: /blog ならカンマ区切りで追加する、BlogTag.tsxに切り出し
-    <Link href={"/blog?tag=" + tag}>
-      <Tag colorScheme={tagColors[tag]}>{tag}</Tag>
-    </Link>
-  );
-};
+import { Flex } from "@chakra-ui/react";
+import { BlogTag } from "./BlogTag";
 
 export const BlogTags = ({ tags }: { tags: string[] | undefined }) => {
   if (tags == undefined) {
